f0bcc3db01d09e8eb876ebe3cfca99c9a0744e3e
Mitchell Pash <mitch@mitchpash.com>
2023-06-08 16:24:13 -0500

    Add starter template packages/files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,26 @@
-import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
-import theme from '@chakra/theme';
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from '@chakra/theme'
+import type { AppProps } from 'next/app'
+import { Inter, Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['700'],
+  variable: '--font-monsterrat',
+})
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-inter',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <main className={`${montserrat.variable} ${inter.variable}`}>
+        <Component {...pageProps} />
+      </main>
     </ChakraProvider>
-  );
+  )
 }
